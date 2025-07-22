5626df947b2a05a6546a3cf2a8b6ec9c3e4ebfee
yuchenlin <yuchenlin0612@gmail.com>
2025-07-23 02:37:17 +0800

    remove useless import

--- scripts/inference_partcrafter.py
+++ scripts/inference_partcrafter.py
@@ -12,7 +12,7 @@ from huggingface_hub import snapshot_download
 from PIL import Image
 from accelerate.utils import set_seed
 
-from src.utils.data_utils import get_colored_mesh_composition, scene_to_parts, load_surfaces
+from src.utils.data_utils import get_colored_mesh_composition
 from src.utils.render_utils import render_views_around_mesh, render_normal_views_around_mesh, make_grid_for_images_or_videos, export_renderings
 from src.pipelines.pipeline_partcrafter import PartCrafterPipeline
 from src.utils.image_utils import prepare_image
